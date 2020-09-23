4d2f09ad69e555035848d49a7e21133c33282b9c
Jose Cortes <jacortes74493@umanizales.edu.co>
2020-09-22 23:15:20 -0500

    connect mongo atlas

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/shop',
+  //// db: process.env.MONGODB_URI || 'mongodb://localhost:27017/shop',
+  db: process.env.MONGODB_URI || 'mongodb+srv://joseCortes:jose123@cluster0.hrafr.mongodb.net/programacion3?retryWrites=true&w=majority',
   SECRET_TOKEN: 'miclavedetokens'
 }
--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const config = require('./config')
 
-mongoose.connect(config.db, (err, res) => {
-  if (err) {
-    return console.log(`Error al conectar a la base de datos: ${err}`)
-  }
-  console.log('Conexión a la base de datos establecida...')
-
-  app.listen(config.port, () => {
-    console.log(`API REST corriendo en http://localhost:${config.port}`)
-  })
+mongoose.connect(config.db,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+  useCreateIndex:true
 })
+.then(db => console.log('Db is conneted'))
+.catch(err => console.log('Error',err))
+
+app.listen(config.port, () => {
+  console.log(`API REST corriendo en http://localhost:${config.port}`)
+})
+
